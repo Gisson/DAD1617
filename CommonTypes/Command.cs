fc43c4b05bb8ea60622a971244b1be8a5dd824d2
Nuno Silva <nuno.m.ribeiro.silva@tecnico.ulisboa.pt>
2016-11-03 17:31:13 +0000

    add remote interface for operator commands
    part of ##18

--- CommonTypes/Command.cs
+++ CommonTypes/Command.cs
@@ -2,85 +2,16 @@
 
 namespace CommonTypes
 {
-    [Serializable] /* Marshal by Value; Note: attributes are not inherited */
-    public abstract class Command
+
+    public interface IRemoteCommands
     {
-        private string _name;
-        public Command(string name) { _name = name; }
-
+        void start(String operatorID);
+        void interval(String operatorID, int milliseconds);
+        void status();
+        void crash(String operatorID, int replicaID);
+        void freeze(String operatorID, int replicaID);
+        void unfreeze(String operatorID, int replicaID);
+        /* the wait command is not remote */
     }
 
-    namespace Commands {
- 
-        [Serializable]
-        public class Start : Command
-        {
-            private string _operatorId;
-            public Start(string opID) : base("Start")
-            {
-                _operatorId = opID;
-            }
-        }
-
-        [Serializable]
-        public class Interval : Command
-        {
-            private string operatoId;
-            private int xMs;
-            public Interval(string opID, int miliseconds) : base("Interval")
-            {
-                operatoId = opID;
-                xMs = miliseconds;
-            }
-        }
-
-        [Serializable]
-        public class Status : Command
-        {
-            public Status() : base("Status") { }
-        }
-
-        [Serializable]
-        public class Crash : Command
-        {
-            private string _proccessID;
-            public Crash(string pid) : base("Crash")
-            {
-                _proccessID = pid;
-            }
-        }
-
-        [Serializable]
-        public class Freeze : Command
-        {
-            private string _proccessID;
-            public Freeze(string pid) : base("Freeze")
-            {
-                _proccessID = pid;
-            }
-        }
-
-        [Serializable]
-        public class UnFreeze : Command
-        {
-            private string _proccessID;
-            public UnFreeze(string pid) : base("UnFreeze")
-            {
-                _proccessID = pid;
-            }
-        }
-
-        [Serializable]
-        public class Wait : Command
-        {
-            private int _xMS;
-            public Wait(int xMS) : base("Wait")
-            {
-                _xMS = xMS;
-            }
-        }
-
-        /* TODO add more commands */
-
-    }
 }
