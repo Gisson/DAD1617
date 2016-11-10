91e597983c2a0e39f127635ea9e75af251c80ee4
Nuno Silva <nuno.m.ribeiro.silva@tecnico.ulisboa.pt>
2016-11-10 19:53:02 +0000

    add Uniq Stream Operator
    closes #21

---
+++ Operator/StreamOperators/Uniq.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator.StreamOperators
+{
+    class Uniq : StreamOperator
+    {
+        List<string> seen;
+        private int field;
+
+        public Uniq(int field)
+        {
+            this.field = field;
+            this.seen = new List<string>();
+        }
+
+        public IList<string> processTuple(IList<string> tuple)
+        {
+            string currentField = tuple[field];
+            if(seen.Contains(currentField))
+            {
+                /* FIXME null is probably not a good idea */
+                return null;
+            } else
+            {
+                seen.Add(currentField);
+                return tuple;
+            }
+        }
+    }
+}
