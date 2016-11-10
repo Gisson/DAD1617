c2cb892ee493d147c498e57357a55c00db3ce65b
Nuno Silva <nuno.m.ribeiro.silva@tecnico.ulisboa.pt>
2016-11-10 19:41:55 +0000

    add Operator Stdin StreamInput for testing
    part of #26

---
+++ Operator/StreamInputs/Stdin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator.StreamInputs
+{
+    class Stdin : StreamInput
+    {
+        public IList<string> getTuple()
+        {
+            string line = Console.ReadLine();
+            string[] words = line.Split(' ');
+            List<string> tuple = new List<string>(words);
+            return tuple;
+        }
+    }
+}
---
+++ Operator/StreamInputs/StreamInput.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Operator.StreamInputs
+{
+    /// <summary>
+    /// interface for a tuple input
+    /// </summary>
+    interface StreamInput
+    {
+        IList<string> getTuple();
+    }
+}
