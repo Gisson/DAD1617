81d95019f31653bd2621388c91ff6b86f53603e1
Nuno Silva <nuno.m.ribeiro.silva@tecnico.ulisboa.pt>
2016-11-10 20:04:13 +0000

    add RoutingPolicy interface and Stdout policy for testing
    part of #7

---
+++ Operator/Routing/Stdout.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator.Routing
+{
+    class Stdout : RoutingPolicy
+    {
+        public void outputTuple(IList<string> tuple)
+        {
+            string line = "TUPLE OUT: ";
+            foreach(string s in tuple)
+            {
+                line += " " + s;
+            }
+            Console.WriteLine(line);
+        }
+    }
+}
