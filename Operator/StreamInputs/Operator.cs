abc772c951276642d0f99c0e8aca1842aaaf4303
Nuno Silva <nuno.m.ribeiro.silva@tecnico.ulisboa.pt>
2016-11-22 16:55:00 +0000

    add Operator InputStream
    * forgot to commit this in 71e89ee9ec01c4635a1dc77098377e474ee0546e
    
    part of #30

---
+++ Operator/StreamInputs/Operator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator.StreamInputs
+{
+    /// <summary>
+    /// InputStream that gets tuples from another operator
+    /// </summary>
+    public class Operator : StreamInput
+    {
+        /// <summary> thread-safe queue of received tuples. </summary>
+        private Queue tupleQueue;
+
+        public Operator()
+        {
+            Queue q = new Queue();
+            tupleQueue = Queue.Synchronized(q);
+        }
+        
+        public IList<string> getTuple()
+        {
+            if(tupleQueue.Count > 0)
+            {
+                return (IList<string>)tupleQueue.Dequeue();
+            } else
+            {
+                /* FIXME should we block instead? */
+                return null;
+            }
+        }
+
+        /* FIXME currently I'm not distinguishing between different operators.
+         * That is, everything that comes from OPx is received by this class. */
+
+        /// <summary> called by OperatorServices when a new tuple is received </summary>
+        public void putTuple(IList<string> tuples)
+        {
+            tupleQueue.Enqueue(tuples);
+        }
+    }
+}
