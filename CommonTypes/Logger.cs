50a79ec459858feca5c6255fc74facb635615e3f
Nuno Silva <nuno.m.ribeiro.silva@tecnico.ulisboa.pt>
2016-12-09 03:37:49 +0000

    Logger: more methods; use locks

--- CommonTypes/Logger.cs
+++ CommonTypes/Logger.cs
@@ -9,24 +9,41 @@ namespace CommonTypes
     public static class Logger
     {
         public static bool debug = false;
+        private static readonly object ConsoleWriterLock = new object();
 
         public static void debugWriteLine(string line)
         {
             if (debug)
             {
-                ConsoleColor old = Console.ForegroundColor;
-                Console.ForegroundColor = ConsoleColor.Cyan;
-                Console.WriteLine("debug: " + line);
-                Console.ForegroundColor = old;
+                lock (ConsoleWriterLock)
+                {
+                    ConsoleColor old = Console.ForegroundColor;
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.WriteLine("debug: " + line);
+                    Console.ForegroundColor = old;
+                }
             }
         }
 
         public static void errorWriteLine(string line)
         {
-            ConsoleColor old = Console.ForegroundColor;
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("error: " + line);
-            Console.ForegroundColor = old;
+            lock (ConsoleWriterLock)
+            {
+                ConsoleColor old = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("error: " + line);
+                Console.ForegroundColor = old;
+            }
+        }
+        
+        public static void errorWriteLine()
+        {
+            errorWriteLine("");
+        }
+
+        public static void debugWriteLine()
+        {
+            debugWriteLine("");
         }
     }
 }
